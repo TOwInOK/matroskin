4b3902c5ef48a3a0999067d99537a0f90e0f0629
TOwInOK 60252419+TOwInOK@users.noreply.github.com <60252419+TOwInOK@users.noreply.github.com>
2025-11-06 14:06:02 +0700

    update deps, add cfgs

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 fn main() {
-    // Copy the images to the output when generating documentation
     println!("cargo:rerun-if-changed=assets/doc");
+    #[cfg_attr(docsrs, feature(doc_cfg))]
     std::fs::copy(".content/logo.webp", "target/doc/logo.ico")
         .expect("Failed to copy crate favicon when building documentation.");
+    #[cfg_attr(docsrs, feature(doc_cfg))]
     std::fs::copy(".content/logo.webp", "target/doc/logo.webp")
         .expect("Failed to copy crate logo when building documentation.");
 }
