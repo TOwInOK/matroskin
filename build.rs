c9973c93b949daa7da29a93b22f64be93d8797e7
TOwInOK 60252419+TOwInOK@users.noreply.github.com <60252419+TOwInOK@users.noreply.github.com>
2025-11-06 14:09:04 +0700

    fix type, add doc linting

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("cargo:rerun-if-changed=assets/doc");
     #[cfg_attr(docsrs, feature(doc_cfg))]
-    std::fs::copy(".content/logo.webp", "target/doc/logo.ico")
+    std::fs::copy(".content/favicon.ico", "target/doc/favicon.ico")
         .expect("Failed to copy crate favicon when building documentation.");
     #[cfg_attr(docsrs, feature(doc_cfg))]
     std::fs::copy(".content/logo.webp", "target/doc/logo.webp")
